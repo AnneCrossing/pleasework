428a71886296659236526c8a68e7c592ae3564d3
Anne !!! <108868747+AnneCrossing@users.noreply.github.com>
2022-11-27 18:24:18 +0000

    fix issue

---
+++ AnneCrossingCafe/Assets/Scripts/fliplooking.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using pathfinding;
+
+public class fliplooking : MonoBehaviour {
+	public AIpath lookingright;
+	bool lookright = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(lookingright.desiredvelocity.x >= 0.01f){
+			lookright==true;
+			transform.Rotate(0, 180, 0);
+			lookright==false;
+		}
+		elseif(lookingright.desiredvelocity.x <= -0.01f) {
+			transform.Rotate(0, 180, 0);
+			lookright==true;
+			transform.Rotate(0, 180, 0);
+			lookright==false;
+		}
+		
+	}
+}
